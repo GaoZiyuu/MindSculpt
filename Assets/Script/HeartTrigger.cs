a93433045dd37a1362f8f1dd6cbeeca28804f424
Gao-Ziyu <gaoziyu528@gmail.com>
2024-01-29 20:37:00 +0800

    Interaction between pills and heart
    - mood stabiliser grabbable
    - when mood stabiliser touch the heart, the chains will play animation (rn just setactive to false)
    - player get close to heart will teleport to another area

---
+++ Assets/Script/HeartTrigger.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartTrigger : MonoBehaviour
+{
+    public Transform tp_point;
+
+    public bool moodDone = false;
+
+    public GameObject chain1;
+    public GameObject chain2;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "mood_stabiliser")
+        {
+            //play animation
+
+            chain1.SetActive(false);
+            chain2.SetActive(false);
+            moodDone = true;
+        }
+
+        if(other.gameObject.tag == "Player" && moodDone)
+        {
+            //tp player
+            TPplayer(other.gameObject);
+        }
+    }
+
+    private void TPplayer(GameObject player)
+    {
+        if (tp_point != null)
+        {
+            // Teleport the player to the specified position
+            player.transform.position = tp_point.position;
+        }
+        else
+        {
+            Debug.LogError("Teleport point is not assigned in the inspector.");
+        }
+    }
+}
